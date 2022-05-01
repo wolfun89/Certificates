e0abaadcd98e78b404cba202c298ff9d49819cd1
wolfun89 <86215697+wolfun89@users.noreply.github.com>
2022-05-01 11:14:29 +0200

    removing unnecessary files

--- HTML CSS based/Always Outdoors landing page(GH)/script.js
+++
@@ -1,7 +0,0 @@
-const hamburger = document.querySelector(".hamburger-div");
-const navLinks = document.querySelector(".main-nav");
-const links = document.querySelectorAll(".main-nav li");
-
-hamburger.addEventListener("click", () => {
-	navLinks.classList.toggle("open");
-})
